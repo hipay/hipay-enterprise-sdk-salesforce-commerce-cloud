0506809f00604dd4bcf60043fe48e82f35b5eb06
abrison <abrison@clever-age.com>
2022-11-28 18:35:48 +0100

    Added Resources controller

--- cartridges/int_hipay_sfra/cartridge/client/default/js/checkout.js
+++ cartridges/int_hipay_sfra/cartridge/client/default/js/checkout.js
@@ -2,6 +2,8 @@
 
 var processInclude = require('base/util');
 
+var checkout = window.SFRA6Enabled === true ? require('./checkout/checkoutSFRA6') : require('./checkout/checkoutSFRA5');
+
 $(document).ready(function () {
-    processInclude(require('./checkout/checkout'));
+    processInclude(checkout);
 });
