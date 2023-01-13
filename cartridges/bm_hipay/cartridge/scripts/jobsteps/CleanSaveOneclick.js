2f063de614724ef7ef92fbe920c833ec59047e56
Julien Kruppa <jkruppa@clever-age.com>
2023-01-13 15:33:12 +0100

    Refactor Jobs

---
+++ cartridges/bm_hipay/cartridge/scripts/jobsteps/CleanSaveOneclick.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/* Script Modules */
+var Logger = require('dw/system/Logger').getLogger('Hipay-stepCleanSaveOneclick');
+var Status = require('dw/system/Status');
+
+var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+var Constants = require('*/cartridge/scripts/util/hipayConstants');
+
+/**
+ * Delete attempts that are more than 24 hours old
+ *
+ * @param {*} options
+ * @param {dw.job.JobStepExecution} jobStepExecution - Job Step Execution
+ * @return {dw.system.Status} Job step execution status
+ */
+function cleanSaveOneclick(options, jobStepExecution) {
+    Logger.info('step-job stepCleanSaveOneclick : start of treatment');
+    try {
+        var listAttempts;
+        var deletedNumber = 0;
+        var failedNumber = 0;
+        var lastDay = new Date();
+        lastDay.setDate(lastDay.getDate() - 1);
+
+        listAttempts = CustomObjectMgr.queryCustomObjects(Constants.OBJ_SAVE_ONE_CLICK, 'custom.attemptDate < {0}', 'custom.attemptDate desc', lastDay);
+
+        if (listAttempts && listAttempts.getCount() > 0) {
+            Logger.info('\n[The number of attempts that are more than 24 hours old : {0} ]', listAttempts.getCount());
+            while (listAttempts.hasNext()) {
+                var currentAttemptObj = listAttempts.next();
+                if (currentAttemptObj) {
+                    //CustomObjectMgr.remove(currentAttemptObj);
+                    logger.info('DEBUG - REMOVE');
+                    deletedNumber++;
+                }
+            }
+
+            if (deletedNumber > 0) {
+                Logger.info('\n[The number of attempts that have been deleted : {0} ]', deletedNumber);
+            }
+
+            failedNumber = listAttempts.getCount() - deletedNumber;
+            if (failedNumber > 0) {
+                Logger.warn('\n### The number of attempts that have failed to remove : {0} ###', failedNumber);
+            }
+        } else {
+            Logger.info('\n###### NO attempts found that have more than 24 hours ######');
+        }
+    } catch (e) {
+        Logger.error('[stepCleanSaveOneclick.js] crashed on line: ' + e.lineNumber + ' with error: ' + e);
+        return new Status(Status.ERROR);
+    }
+
+    Logger.info('step-job stepCleanSaveOneclick : end with OK');
+    return new Status(Status.OK);
+}
+
+exports.execute = cleanSaveOneclick;
