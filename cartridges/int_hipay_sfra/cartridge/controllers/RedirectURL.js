e7602ed3d6dc9a1160964d4ad20de4595404fd75
abrison <abrison@clever-age.com>
2023-04-25 09:32:07 +0200

    [HP-65] Apple developer merchant id

---
+++ cartridges/int_hipay_sfra/cartridge/controllers/RedirectURL.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var server = require('server');
+
+server.extend(module.superModule);
+
+var Site = require('dw/system/Site');
+
+server.prepend('Start', function (req, res, next) {
+    var URLRedirectMgr = require('dw/web/URLRedirectMgr');
+    const appleMerchantId = Site.getCurrent().getCustomPreferenceValue("hipayAppleMerchantID");
+    var test = URLRedirectMgr.getRedirectOrigin();
+    if (URLRedirectMgr.getRedirectOrigin() === '/.well-known/apple-developer-merchantid-domain-association' && !empty(appleMerchantId)) {
+        res.setContentType('text/plain');
+        res.print(appleMerchantId);
+        this.done(req, res);
+        return;
+    }
+
+    next();
+});
+
+module.exports = server.exports();
