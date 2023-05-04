e2341bb720d434268e90c1c12039f291248a4174
abrison <abrison@clever-age.com>
2023-05-04 10:43:49 +0200

    [HP-65] Removed variable test

--- cartridges/int_hipay_sfra/cartridge/controllers/RedirectURL.js
+++ cartridges/int_hipay_sfra/cartridge/controllers/RedirectURL.js
@@ -9,7 +9,6 @@ var Site = require('dw/system/Site');
 server.prepend('Start', function (req, res, next) {
     var URLRedirectMgr = require('dw/web/URLRedirectMgr');
     const appleMerchantId = Site.getCurrent().getCustomPreferenceValue("hipayAppleMerchantID");
-    var test = URLRedirectMgr.getRedirectOrigin();
     if (URLRedirectMgr.getRedirectOrigin() === '/.well-known/apple-developer-merchantid-domain-association' && !empty(appleMerchantId)) {
         res.setContentType('text/plain');
         res.print(appleMerchantId);
